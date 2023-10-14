39ff0ce998503f1738ef8dfd0c386d3da0b38152
Nils <nils.mathieu.contact@gmail.com>
2023-10-14 03:43:52 +0200

    move the core project to the 'warm-core' subcrate

---
+++ crates/core/src/lib.rs
@@ -0,0 +1,9 @@
+//! The core rendering framework for the `warm` rendering engine.
+
+#![warn(missing_docs, missing_debug_implementations)]
+#![deny(unsafe_op_in_unsafe_fn)]
+
+pub mod gpu;
+pub mod surface;
+
+mod utility;
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
 //! A lightweight rendering engine built on top of Vulkan.
-
-#![warn(missing_docs, missing_debug_implementations)]
-#![deny(unsafe_op_in_unsafe_fn)]
-
-pub mod gpu;
-pub mod surface;
-
-mod utility;
