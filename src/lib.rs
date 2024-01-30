209b61443f9f759edb1d838f5a961688b8d288ba
nils-mathieu <nils.mathieu.contact@gmail.com>
2024-01-30 12:22:17 +0100

    load Vulkan

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
 //! A thin wrapper around the Vulkan library, based on [`ash`].
+
+mod library;
+pub use library::*;
---
+++ src/library.rs
@@ -0,0 +1,106 @@
+use std::path::Path;
+use std::sync::Arc;
+
+use ash::vk;
+
+/// An error that might occur while creating a [`Library`] instance.
+#[derive(Debug, thiserror::Error)]
+pub enum LibraryError {
+    #[error("failed to load the Vulkan library: {0}")]
+    Load(libloading::Error),
+    #[error("no entry point found in the loaded Vulkan library")]
+    MissingEntryPoint,
+}
+
+/// A collection of Vulkan functions that have been loaded from a loaded Vulkan library.
+///
+/// The function pointers in this struct are only valid as long as the library they point into
+/// remains loaded in the current program's memory.
+#[derive(Debug, Clone)]
+pub struct LibraryFns {
+    pub get_instance_proc_addr: vk::PFN_vkGetInstanceProcAddr,
+}
+
+impl LibraryFns {
+    /// Creates a new [`LibraryFns`] instance from the provided `vk::PFN_vkGetInstanceProcAddr`.
+    ///
+    /// # Safety
+    ///
+    /// The provided entry point must comply with the Vulkan specification.
+    unsafe fn load(ep: vk::PFN_vkGetInstanceProcAddr) -> Self {
+        Self {
+            get_instance_proc_addr: ep,
+        }
+    }
+}
+
+/// The Vulkan library, dynamically loaded.
+///
+/// As long as this struct is alive, the library is loaded into the current program's memory and
+/// can be used to create static Vulkan objects and query global Vulkan properties.
+///
+/// This struct is meant to be used through an [`Arc<T>`] to ensure that it is not dropped while
+/// still in use.
+pub struct Library {
+    /// The inner library object.
+    ///
+    /// Dropping this field will unload the library from the current program's memory.
+    _inner: libloading::Library,
+
+    /// The list of functions loaded from the library.
+    fns: LibraryFns,
+}
+
+impl Library {
+    /// Creates a new [`Library`] instance from the provided [`libloading::Library`].
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that `inner` defines a valid implementation of the Vulkan API (as
+    /// defined by the Vulkan specification).
+    pub unsafe fn from_library(inner: libloading::Library) -> Result<Arc<Self>, LibraryError> {
+        let ep = *inner
+            .get(b"vkGetInstanceProcAddr\0")
+            .map_err(|_| LibraryError::MissingEntryPoint)?;
+        Ok(Arc::new(Self {
+            _inner: inner,
+            fns: LibraryFns::load(ep),
+        }))
+    }
+
+    /// Creates a new [`Library`] instance by loading the Vulkan library located at the provided
+    /// path.
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that, if `path` points to a valid dynamic library, that library
+    /// defines a valid implementation of the Vulkan API (as defined by the Vulkan specification).
+    pub unsafe fn from_path(path: impl AsRef<Path>) -> Result<Arc<Self>, LibraryError> {
+        let inner = libloading::Library::new(path.as_ref()).map_err(LibraryError::Load)?;
+        Self::from_library(inner)
+    }
+
+    /// Loads a [`Library`] instance from the default Vulkan path for the current platform.
+    ///
+    /// # Safety considerations
+    ///
+    /// Loading an external library is inherently unsafe, as it can lead to arbitrary code
+    /// execution. This method is not marked as 'unsafe' because as long as the user does not
+    /// have a corrupted or malicious Vulkan library in the default path, this method is safe.
+    ///
+    /// If, by any chance, the user has a spiked library, they probably have bigger problems than
+    /// this method ending up being unsafe.
+    pub fn new() -> Result<Arc<Self>, LibraryError> {
+        #[cfg(target_os = "linux")]
+        const LIBRARY_PATH: &str = "libvulkan.so.1";
+
+        // SAFETY: see the safety considerations of this method.
+        unsafe { Self::from_path(LIBRARY_PATH) }
+    }
+
+    /// Returns the list of function pointers loaded from the library.
+    #[inline(always)]
+    pub fn fns(&self) -> &LibraryFns {
+        &self.fns
+    }
+}
